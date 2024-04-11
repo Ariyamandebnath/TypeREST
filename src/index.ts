bf30a453e6515e8a8a20c365a690da95a84350ee
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-04-11 20:50:26 +0530

    Database connected

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import dotenv from "dotenv";
 import { app } from "./app";
+import connectDB from "./db/index";
 
 dotenv.config({
     path: './.env'
 });
 
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
+        app.on("error", (err) => {
+            console.error("Server error: ", err);
+        });
+    })
+
+    .catch((err) => {
+        console.error(`mongoDB connection failed: ${err.message}`);
+    })
