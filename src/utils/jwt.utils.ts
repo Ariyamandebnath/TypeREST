89055c6b263ba1bd134ef5b3387a7e622463c491
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-04-12 12:23:43 +0530

    added all  utills

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,41 @@
+import jwt from "jsonwebtoken";
+
+export function signJwt(
+  object: Object,
+  keyName: "accessTokenPrivateKey" | "refreshTokenPrivateKey",
+  options?: jwt.SignOptions | undefined
+) {
+  const signingKey = Buffer.from(
+   `${process.env.ACCESS_TOKEN_PRIVATE_KEY}`
+  ).toString("ascii");
+
+  return jwt.sign(object, signingKey, {
+    ...(options && options),
+    algorithm: "RS256",
+  });
+}
+
+export function verifyJwt(
+  token: string,
+  keyName: "accessTokenPublicKey" | "refreshTokenPublicKey"
+) {
+  const publicKey = Buffer.from(`${process.env.ACCESS_TOKEN_PUBLIC_KEY}`, "base64").toString(
+    "ascii"
+  );
+
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    return {
+      valid: true,
+      expired: false,
+      decoded,
+    };
+  } catch (e: any) {
+    console.error(e);
+    return {
+      valid: false,
+      expired: e.message === "jwt expired",
+      decoded: null,
+    };
+  }
+}
