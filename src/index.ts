275d9dbd512f4b52f089128607aa67b720f90b7e
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-04-12 14:14:54 +0530

    Api completed

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,49 @@
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";import connectDB from "./db/index";
-import logger from "./logger/winston.logger"
+import express, { Request, Response } from "express";
+import connectDB from "./db/index";
+import logger from "./logger/winston.logger";
 import responseTime from "response-time";
-
-
+import routes from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
+import { restResponseTimeHistogram, startMetricsServer } from "./utils/metrics";
+import swaggerDocs from "./utils/swagger";
 
 dotenv.config({
     path: './.env'
 });
 
-logger.info("information log")
+const app = express();
+const port = parseInt(process.env.PORT ?? "3000");
 
+app.use(express.json());
+app.use(deserializeUser);
+
+app.use(
+  responseTime((req: Request, res: Response, time: number) => {
+    if (req?.route?.path) {
+      restResponseTimeHistogram.observe(
+        {
+          method: req.method,
+          route: req.route.path,
+          status_code: res.statusCode,
+        },
+        time * 1000
+      );
+    }
+  })
+);
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server is running at port : ${process.env.PORT}`);
-        });
-        app.on("error", (err) => {
-            logger.error("Server error: ", err);
+        app.listen(port, () => {
+            logger.info(`Server is running at port: ${port}`);
+            routes(app);
+            startMetricsServer();
+            swaggerDocs(app, port);
         });
 
-        
+        logger.info("Server started successfully.");
     })
-
     .catch((err) => {
-        logger.error(`mongoDB connection failed: ${err.message}`);
-    })
+        logger.error(`MongoDB connection failed: ${err.message}`);
+    });
